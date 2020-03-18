140d61cdfad4c8eaff5978d754a880212dfdf7d0
William Yeung <williamyeung1141998@gmail.com>
2020-03-17 23:17:36 -0400

    Add random show funtionality

--- BingeBot/ViewController.swift
+++ BingeBot/ViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var showsLabel: UILabel!
+    @IBOutlet weak var randomShowLabel: UILabel!
+    @IBOutlet weak var bingeBotSpokenLabel: UILabel!
+    @IBOutlet weak var addShowTextField: UITextField!
+    @IBOutlet weak var randomShowStackView: UIStackView!
+    @IBOutlet weak var addShowStackView: UIStackView!
+    @IBOutlet weak var showsStackView: UIStackView!
+    
+    
+    var shows = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        showsStackView.isHidden = true
+        randomShowStackView.isHidden = true
     }
 
-
+    @IBAction func bingeButton(_ sender: Any) {
+        randomShowLabel.text = shows.randomElement()
+        randomShowLabel.isHidden = false
+        bingeBotSpokenLabel.isHidden = false
+    }
+    
+    
+    func updateShowsLabel() {
+        showsLabel.text = shows.joined(separator: ", ")
+    }
+    
+    @IBAction func addShowBtnWasPressed(_ sender: Any) {
+        guard let showName = addShowTextField.text else { return }
+        
+        if (addShowTextField.text!.count >= 1) {
+            shows.append(showName)
+            updateShowsLabel()
+            addShowTextField.text = ""
+            
+            showsStackView.isHidden = false
+            
+            if shows.count > 1 {
+                randomShowStackView.isHidden = false
+                bingeBotSpokenLabel.isHidden = true
+                randomShowLabel.isHidden = true
+            }
+        }
+    }
+    
 }
 
